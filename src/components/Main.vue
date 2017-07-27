f791558ce980791f8f2c244cab9178aabbf19470
moonspam <booboo0412@gmail.com>
2017-07-27 18:34:51 +0900

    최신 돌픽 Api 호출 성공

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,36 +1,22 @@
 <template>
-  <div class="gbox_gallery">
-    <div class="dp_title">
-      <strong onClick="alert($(this).text());">최신 DolPic</strong>
-      <span class="tbox">더 보기</span>
+  <div class='gbox_gallery'>
+    <div class='dp_title'>
+      <strong onClick='alert($(this).text());'>최신 DolPic</strong>
+      <span class='tbox'>더 보기</span>
     </div>
-    <div class="gbox" style="background:url(https://pbs.twimg.com/media/B0r8fJmCIAErsoR.jpg) center center no-repeat;background-size:cover"><div class="gbox_thumb g_t_twitter"><div class="gbox_t_title">@써니</div><div class="gbox_t_text"><img src="../assets/img/icon_like.png" height="9" alt="like">1,450</div></div></div>
-    <div class="gbox" style="background:url(https://pbs.twimg.com/media/B8bQ7yrCcAE9-vl.jpg) center center no-repeat;background-size:cover"><div class="gbox_thumb g_t_facebook"><div class="gbox_t_title">@써니</div><div class="gbox_t_text"><img src="../assets/img/icon_like.png" height="9" alt="like">1,450</div></div></div>
-    <div class="gbox" style="background:url(https://pbs.twimg.com/media/B6Q4dOqCUAAPIV3.jpg) center center no-repeat;background-size:cover"><div class="gbox_thumb g_t_instagram"><div class="gbox_t_title">@써니</div><div class="gbox_t_text"><img src="../assets/img/icon_like.png" height="9" alt="like">1,450</div></div></div>
-    <div class="gbox" style="background:url(https://pbs.twimg.com/media/B0r8fJmCIAErsoR.jpg) center center no-repeat;background-size:cover"><div class="gbox_thumb g_t_twitter"><div class="gbox_t_title">@써니</div><div class="gbox_t_text"><img src="../assets/img/icon_like.png" height="9" alt="like">1,450</div></div></div>
-    <div class="gbox" style="background:url(https://pbs.twimg.com/media/B8bQ7yrCcAE9-vl.jpg) center center no-repeat;background-size:cover"><div class="gbox_thumb g_t_facebook"><div class="gbox_t_title">@써니</div><div class="gbox_t_text"><img src="../assets/img/icon_like.png" height="9" alt="like">1,450</div></div></div>
-    <div class="gbox" style="background:url(https://pbs.twimg.com/media/B6Q4dOqCUAAPIV3.jpg) center center no-repeat;background-size:cover"><div class="gbox_thumb g_t_instagram"><div class="gbox_t_title">@써니</div><div class="gbox_t_text"><img src="../assets/img/icon_like.png" height="9" alt="like">1,450</div></div></div>
-    <div class="gbox" style="background:url(https://pbs.twimg.com/media/B0r8fJmCIAErsoR.jpg) center center no-repeat;background-size:cover"><div class="gbox_thumb g_t_twitter"><div class="gbox_t_title">@써니</div><div class="gbox_t_text"><img src="../assets/img/icon_like.png" height="9" alt="like">1,450</div></div></div>
-    <div class="gbox" style="background:url(https://pbs.twimg.com/media/B8bQ7yrCcAE9-vl.jpg) center center no-repeat;background-size:cover"><div class="gbox_thumb g_t_facebook"><div class="gbox_t_title">@써니</div><div class="gbox_t_text"><img src="../assets/img/icon_like.png" height="9" alt="like">1,450</div></div></div>
-    <div class="gbox" style="background:url(https://pbs.twimg.com/media/B6Q4dOqCUAAPIV3.jpg) center center no-repeat;background-size:cover"><div class="gbox_thumb g_t_instagram"><div class="gbox_t_title">@써니</div><div class="gbox_t_text"><img src="../assets/img/icon_like.png" height="9" alt="like">1,450</div></div></div>
-    <div class="gbox" style="background:url(https://pbs.twimg.com/media/B0r8fJmCIAErsoR.jpg) center center no-repeat;background-size:cover"><div class="gbox_thumb g_t_twitter"><div class="gbox_t_title">@써니</div><div class="gbox_t_text"><img src="../assets/img/icon_like.png" height="9" alt="like">1,450</div></div></div>
-    <div class="clear"></div>
-
-    <!-- 갤러리 네비게이션 -->
-    <ul class="gbox_list">
-      <li class="on">1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-      <li>6</li>
-      <li>7</li>
-      <li>8</li>
-      <li>9</li>
-      <li>&gt;</li>
-    </ul>
+    <div v-for='post in posts' :key='post._id'>
+      <div class='gbox' v-bind:style="{ backgroundImage: 'url(' + post.url + ')' }">
+        <div class='gbox_thumb' :class="[{g_t_twitter: post.urlType === 1}, {g_t_instagram: post.urlType === 2}, {g_t_facebook: post.urlType === 3}]">
+          <div class='gbox_t_title'>{{post.hashTagId.twitterHashTag}}</div>
+          <div class='gbox_t_text'>
+            <img src='../assets/img/icon_like.png' height='9' alt='like'>{{post.likeCount}}
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class='clear'></div>
     <!-- 구글 광고 -->
-    <img src="../assets/img/sample_ad2.jpg" width="728" height="90">
+    <img src='../assets/img/sample_ad2.jpg' width='728' height='90'>
   </div>
 </template>
 
@@ -39,6 +25,20 @@ export default {
   name: 'main',
   data () {
     return {
+      active: false,
+      posts: []
+    }
+  },
+  created () {
+    this.listNew()
+  },
+  methods: {
+    listNew () {
+      const apiURL = 'http://dolpic.kr/api/listNew'
+      this.$http.post(apiURL).then((result) => {
+        console.log(result)
+        this.posts = result.data
+      })
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 import 'expose-loader?$!expose-loader?jQuery!jquery'
 require('./assets/css/common.css')
 
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
 
 /* eslint-disable no-new */
 new Vue({
