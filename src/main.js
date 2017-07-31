ac4ebe8f5a9df358f8ed19432498cd6f9da27770
moonspam <booboo0412@gmail.com>
2017-07-31 14:47:06 +0900

    remove jquery code

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
-import 'expose-loader?$!expose-loader?jQuery!jquery'
 require('./assets/css/common.css')
 
 Vue.config.productionTip = false
